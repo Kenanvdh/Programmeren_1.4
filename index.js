95bc1362b072787efe53e81f615cd33ce4d60239
Kenanvdh <kenanvdh@ziggo.nl>
2023-05-12 09:06:39 +0200

    Port changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const logger = require('./src/util/utils').logger;
 const routes = require('./src/routes/user.routes')
 
